15f0374b77c9bfda1fd42401b0f923623f2afe0e
casemattersAles76 <44009231+alesToniolo76@users.noreply.github.com>
2020-05-02 20:04:14 -0400

    Updated Code

---
+++ src/app.js
@@ -0,0 +1,29 @@
+/* Script to submit a message in smtpjs as email */
+
+const formFields = document.querySelector(".formFields");
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+    const name = document.querySelector("#name");
+    const subject = document.querySelector("#subject")
+    const email = document.querySelector("#email");
+    const Message = document.querySelector("#message");
+    const errors = ['You must provide a name!', 'You must provide an email!', 'You must provide a message!'];
+    const fields = [name, email, Message];
+
+    for (let i=0; i<errors.length; i++;) {
+        if (fields[i].value = "") {
+          $("#errorList").append('<li>$(errors[i])</li>');
+          $("#errorList").style.display=block;
+          console.log('test');
+      } else {
+        Email.send({
+        SecureToken : "6a6904e6-3f8a-499c-96dd-54e0d96ca7a9",
+        To : 'atoniolo76@gmail.com',
+        From : "atoniolo76@gmail.com",
+        Subject : 'Form submission: ${subject}',
+        Body : "This is an form submission off of the official Sushi Yoshino website<br>  From ${name} at ${email}:<br>${Message}"
+    }).then();
+ }
+}
+}
