2667d7499758bb4b29c01cc1bba320db72579620
Toniolo <alessiotoniolo25@marist.com>
2020-05-06 15:24:04 -0400

    Finished Website

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /* Script to submit a message in smtpjs as email */
 
-const formFields = document.querySelector(".formFields");
+/* const formFields = document.querySelector(".formFields");
 
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -26,4 +26,23 @@ myForm.addEventListener("submit", (e) => {
     }).then();
  }
 }
+}
+*/
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the header
+var header = document.getElementsByClassName("cover-nav");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
 }
